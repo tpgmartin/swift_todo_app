78d85dd3f58eae161e4827962a89f808a734ee14
Thomas Martin <tpgmartin@gmail.com>
2014-07-16 08:32:36 +0100

    Added readme, updated views on storyboard, created taskmanager file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+This repo represents my take on rm2kdev's ["Swift Tutorial - To Do List App"](http://youtu.be/war0gHL26ns?list=UUJvrLzbg4VPRxzf2vhW7G4A). I cannot promise functionality or completeness at this point as, unsuprisingly, Swift is new to me, and somewhat more suprisingly, so is the Xcode environment. 
--- Todo App/FirstViewController.swift
+++ Todo App/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,23 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int{
+        return taskMgr.tasks.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) ->
+        UITableViewCell!{
+            
+            let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "test")
+            
+            cell.text = taskMgr.tasks[indexPath.row].name
+            
+            return cell
+    
+    }
+    
+    
 
 }
 
